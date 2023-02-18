0a52789baae330c6bdcc2a7ab7b892d7d360f7ae
Salam Rahal-Arabi <strarabi@gmail.com>
2023-02-18 14:20:47 -0800

    More work on extension frontend and client side convex

--- extension/public/content-script.js
+++ extension/public/content-script.js
@@ -12,14 +12,7 @@ function makeApiCall() {
 			// handle the response here
 			let responseText = (JSON.parse(openAI_request.response)).choices[0].text
 			console.log(responseText)
-			var convex_request = new XMLHttpRequest()
-			convex_request.open("POST", "https://tremendous-rail-263.convex.site/postResponse", true)
-			convex_request.setRequestHeader("Content-Type", "application/json");
-			convex_request.setRequestHeader("accept", "*/*");
-			convex_request.send(JSON.stringify({
-				url: page_link,
-				table: responseText
-			}))
+			const convexClient = new convex.ConvexHttpClient("https://tremendous-rail-263.convex.site")
 		}
 	}
 
@@ -28,7 +21,8 @@ function makeApiCall() {
 		"prompt": "Generate a table by parsing the assignment names, due dates, and course names (if applicable) from the following text: "
 		+ page_text + "\n\n\nIf you don't see an assignment, due date, or course name, leave the table entry blank" + 
 		" - do not guess. Do not include any information from the text other than the course names, assignment names, and due dates." +
-		"Return the table as a comma separated list, beginning with the headings Course Name,Assignment Name,Due Dates",
+		"To signify an empty entry, use an empty set of quotes." +
+		"Return the table as a comma separated list with each entry on a new line, beginning with the headings Course Name,Assignment Name,Due Dates",
 		"temperature": 0,
 		"max_tokens": 999,
 		"top_p": 1.0,
@@ -36,3 +30,6 @@ function makeApiCall() {
 		"presence_penalty": 0.0
 	}))
 }
+
+export default makeApiCall;
+
--- extension/public/index.js
+++ extension/public/index.js
@@ -1,4 +1,6 @@
+import makeApiCall from "./content-script.js"
 
+// makeApiCallTitle()
 window.onload = function() {	
 	let scrape_button = document.getElementById("scrape-button")
 	scrape_button.addEventListener('click', function() {
