09e4de2d5bca52a6a65dba90cdc0d2ebf269c3c2
Salam Rahal-Arabi <strarabi@gmail.com>
2023-02-18 16:32:03 -0800

    Fix convex requests when user clicks scrape button

--- extension/public/index.js
+++ extension/public/index.js
@@ -1,6 +1,6 @@
 import makeApiCall from "./content-script.js"
-
-// makeApiCallTitle()
+import makeApiCallTitle from "./title-script.js"
+makeApiCallTitle()
 window.onload = function() {	
 	let scrape_button = document.getElementById("scrape-button")
 	scrape_button.addEventListener('click', function() {
